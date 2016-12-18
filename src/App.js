adced629ffee53e303872ec2f565a4aafd24e84f
Josh Cheek <josh.cheek@gmail.com>
2016-12-18 13:10:19 -0600

    Copy react-grid experiment into the root

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    const list     = this.props.state.list
+    const featured = list.find(p => p.featured) || list[0]
+    return <div className="App">
+      <div className="FeaturedPanel">
+        <FeaturedMedia participant={featured} />
+      </div>
+      <div className="Separator" />
+      <div className="ListPanel">
+        <MediaList participants={list} setFeatured={this.props.setFeatured}/>
+      </div>
+    </div>
+  }
+}
+
+
+class FeaturedMedia extends Component {
+  render() {
+    let media
+    if(this.props.participant)
+      media = <Media participant={this.props.participant} />
+    return <div className="Featured">{media}</div>
+  }
+}
+
+class MediaList extends Component {
+  render() {
+    // console.log(this.props.participants)
+    const media = this.props.participants.map(p =>
+      <Media key={p.identity} participant={p} setFeatured={this.props.setFeatured}/>
+    )
+    return <div className="List">{media}</div>
+  }
+}
+
+class Media extends Component {
+  render() {
+    const participant = this.props.participant
+    const url = participant.media.url
+    return <img
+      className="Media"
+      alt={participant.identity}
+      src={url}
+      onClick={() => this.props.setFeatured(this.props.participant)}
+    />
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+function newMedia(name) {
+  // figure out what this thing should look like
+  // probably an attach and detach method
+  return {
+    url: `https://dummyimage.com/320x240/000000/ffffff?text=${name}`
+  }
+}
+
+function newParticipant(id, name) {
+  return {
+    identity:  id,
+    media:     newMedia(name),
+    connected: true,
+  }
+}
+
+let participants = [
+  newParticipant(1, 'josh-ren'),
+  newParticipant(2, 'josh-pumpkin'),
+  newParticipant(3, 'josh-ren-blu'),
+  // newParticipant(4, 'josh-ren-blu'),
+  // newParticipant(5, 'josh-ren-blu'),
+  // newParticipant(6, 'josh-ren-blu'),
+]
+let state = { type: "mediaList", list: participants }
+
+const dom = ReactDOM.render(
+  <App state={state} setFeatured={setFeatured}/>,
+  document.getElementById('root')
+);
+
+function setFeatured(participant) {
+  participants.forEach(p => p.featured = false)
+  participant.featured = true
+  dom.forceUpdate()
+}
+
+
+// maybe something like this:
+
+/*
+
+// -----  error  -----
+// that first if statement
+{ type: "error", reason: "WebRTC is not available in your browser." }
+
+// eg connection issue
+{ type: "error", reason: e.toString() }
+
+
+// -----  disconnected  -----
+{ type: "disconnected" }
+room.on('disconnected', function () { ... })
+
+
+// -----  mediaList  -----
+{ type: "mediaList", list: participants }
+
+// media.isMuted, media.isPaused
+// room.localParticipant.media.attach('#local-media');
+// participant.media.detach() // what would happen if we didn't?
+function newParticipant(p) {
+  // should also put whether it's local or not
+  return { identity: p.identity, media: p.media, connected: true }
+}
+
+let participants = room.participants
+                       .concat(room.localParticipant)
+                       .map(newParticipant)
+
+
+room.on('participantConnected', p => {
+  participants = participants.concat(newParticipant(p))
+  // somehow force rerender here
+})
+
+
+// https://facebook.github.io/react/docs/react-component.html#forceupdate
+room.on('participantDisconnected', function (p) {
+});
+
+// -----  ??  -----
+// figure out what this event is, does it mean we're navigating away from the page?
+window.addEventListener('beforeunload', function() {
+  room.disconnect()
+})
+*/

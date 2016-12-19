efc328f1479007940b084147724879d3083e8a7b
Josh Cheek <josh.cheek@gmail.com>
2016-12-19 16:49:22 -0600

    Refactor the chatroom endpoint

--- app/slackbot_videochat.rb
+++ app/slackbot_videochat.rb
@@ -3,48 +3,29 @@ require 'digest/md5'
 require 'twilio-ruby'
 
 class SlackbotVideochat < Sinatra::Base
-  # Whenever someone says '/videochat'
-  # Slack posts to this url (see test for keys)
-  # I'm not sure where the docs are for more interesting responses -.-
-  post '/videochats' do
-    content_type :json
-    url = File.join request.url, unique_token
-    { parse: 'full',
-      attachments: [
-        { text: "<@#{params[:user_id]}> invites you to <#{url}|videochat>!",
-          color: "#AAAA66",
-        }
-      ]
-    }.to_json
-  end
-
   get '/' do
     erb :main
   end
 
+  # Slack posts to this url (see test for keys)
+  # https://api.slack.com/docs/message-formatting
+  # https://api.slack.com/docs/message-attachments
+  post '/videochats' do
+    content_type :json
+    url = File.join request.url, unique_token
+    JSON.dump parse: 'full',
+              attachments: [
+                { text: "<@#{params[:user_id]}> invites you to <#{url}|videochat>!",
+                  color: "#AAAA66",
+                }
+              ]
+  end
+
   get '/videochats/:room' do
-    # Ideally we could use their name from Slack
-    # probably not possible, for now we'll just give them a random unique value
-    identity = unique_token
-
-    user = Twilio::Util::AccessToken.new(
-      key(:twilio, :account_sid),
-      key(:twilio, :api_key),
-      key(:twilio, :api_secret),
-      3600, # 1 hour
-      identity
-    )
-
-    # Grant access to Video
-    grant = Twilio::Util::AccessToken::VideoGrant.new
-    grant.configuration_profile_sid = key(:twilio, :configuration_sid)
-    user.add_grant grant
-
+    @identity  = unique_token # A random value since we can't get their name from Slack
     @room_name = params[:room]
-    @identity  = identity
-    @token     = user.to_jwt
+    @token     = twilio_token(identity: @identity, duration_seconds: 3600)
     @devmode   = self.class.development? && ('dev' == params[:room])
-
     erb :videochat
   end
 
@@ -59,5 +40,23 @@ class SlackbotVideochat < Sinatra::Base
 
   def key(scope, name)
     env.fetch(:keys).fetch(scope).fetch(name)
+  end
+
+  def twilio_token(identity:, duration_seconds:)
+    token = Twilio::Util::AccessToken.new(
+      key(:twilio, :account_sid),
+      key(:twilio, :api_key),
+      key(:twilio, :api_secret),
+      duration_seconds,
+      identity,
+    )
+    token.add_grant video_grant
+    token.to_jwt # https://jwt.io/
+  end
+
+  def video_grant
+    grant = Twilio::Util::AccessToken::VideoGrant.new
+    grant.configuration_profile_sid = key(:twilio, :configuration_sid)
+    grant
   end
 end

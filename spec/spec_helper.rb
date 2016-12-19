bfb2e908e81fa0362ee989d397b8b9f99219ff7d
Josh Cheek <josh.cheek@gmail.com>
2016-12-19 15:00:12 -0600

    Default RSpec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,9 @@ require 'rack/test'
 TestInternetSession = Struct.new :app do
   include Rack::Test::Methods
 end
+
+RSpec.configure do |config|
+  config.fail_fast = true
+  config.color     = true
+  config.formatter = 'documentation'
+end

4dc3cfcc27f403ca59f7d7745e932feeaddaf1f4
Josh Cheek <josh.cheek@gmail.com>
2016-12-21 16:28:11 -0600

    Responds to Slack with a message

---
+++ app/videochat_app.rb
@@ -0,0 +1,31 @@
+require 'sinatra/base'
+require 'json'
+
+class VideochatApp < Sinatra::Base
+
+  # =====  This is what Slack sends:  =====
+  #   POST / HTTP/1.1
+  #   User-Agent: Slackbot 1.0 (+https://api.slack.com/robots)
+  #   Host: 565969aa.ngrok.io
+  #   Accept-Encoding: gzip,deflate
+  #   Accept: application/json,*/*
+  #   Content-Length: 283
+  #   Content-Type: application/x-www-form-urlencoded
+  #   X-Forwarded-Proto: https
+  #   X-Forwarded-For: 54.89.92.4
+  #
+  #   token=Z4n2e1acd0Iu7jkiHx0Ng21L&team_id=T3BS49M8A&team_domain=module5&channel_id=C3FDDV63V&channel_name=josh-testing&user_id=U3G2NE4MC&user_name=paddington&command=%2Fvc2&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT3BS49M8A%2F119906407301%2FNr4MLEysnZyEGWGscNSpuljZ
+  post '/videochats' do
+    url = 'http://example.org/videochats/somechat'
+    response = {
+      "response_type": "in_channel",
+      "attachments": [
+        {
+          "text": "<@#{params[:user_id]}> has invided you to <#{url}|videochat>",
+          "color": "#FF00FF"
+        }
+      ]
+    }
+    JSON.dump(response)
+  end
+end
--- spec/videochat_spec.rb
+++ spec/videochat_spec.rb
@@ -1,11 +1,41 @@
-# require 'videochat_app'
+app_path = File.expand_path '../app', __dir__
+$LOAD_PATH.unshift app_path
+require 'videochat_app'
+require 'rack/test'
 
 RSpec.describe 'VideochatApp' do
+  include Rack::Test::Methods
+
+  def app
+    VideochatApp
+  end
+
   describe 'when Slack POSTs to /videochats' do
-    it 'responds with a link to a url that the users can videochat'
+    it 'responds with a link to a url that the users can videochat' do
+      from_slack = {
+        token:         "Z4n2e1acd0Iu7jkiHx0Ng21L",
+        team_id:       "T3BS49M8A",
+        team_domain:   "module5",
+        channel_id:    "C3FDDV63V",
+        channel_name:  "josh-testing",
+        user_id:       "UJOSHUSERID",
+        user_name:     "paddington",
+        command:       "%2Fvc2",
+        text:          "",
+        response_url:  "https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT3BS49M8A%2F119906407301%2FNr4MLEysnZyEGWGscNSpuljZ",
+      }
+
+      response = post '/videochats', from_slack
+
+      expect(response).to be_ok
+      expect(response.body).to include "<@UJOSHUSERID> has invided you to <http://example.org/videochats/"
+    end
+
+    it 'responds with valid JSON'
 
     describe 'the videochat link' do
       it 'is on the app\'s host'
+      it 'respects the scheme'
       it 'is a unique url so that users don\'t all wind up in the same chat'
     end
   end
